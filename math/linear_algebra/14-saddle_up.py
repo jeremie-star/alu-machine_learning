61817d16413293b8009369ce523b36b45be0c067
jeremie-star <j.iyamurinz@alustudent.com>
2026-01-28 03:16:06 -0800

    ft: added the read me file

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -8,15 +8,14 @@ Returns a new matrix or None if multiplication is not possible.
 import numpy as np
 
 def np_matmul(mat1, mat2):
-    """
-    Perform matrix multiplication between two numpy arrays.
+   # """
+   #Perform matrix multiplication between two numpy arrays.
+   # Args:
+   #    mat1 (numpy.ndarray): First matrix.
+   #   mat2 (numpy.ndarray): Second matrix.
 
-    Args:
-        mat1 (numpy.ndarray): First matrix.
-        mat2 (numpy.ndarray): Second matrix.
-
-    Returns:
-        numpy.ndarray or None: Result of mat1 x mat2 if shapes are compatible, else None.
-    """
+   #Returns:
+   #    numpy.ndarray or None: Result of mat1 x mat2 if shapes are compatible, else None.
+   # """
     return np.matmul(mat1, mat2)
 
