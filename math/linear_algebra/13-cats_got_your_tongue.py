444784e64aee606c9efd1aa6548aa75b94386515
jeremie-star <j.iyamurinz@alustudent.com>
2026-01-28 03:35:08 -0800

    ft: fix

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -6,6 +6,7 @@ Returns a new matrix or None if multiplication is not possible.
 """
 import numpy as np
 
+
 def np_cat(mat1, mat2, axis=0):
 
     """Concatenates two numpy arrays along a specified axis."""
