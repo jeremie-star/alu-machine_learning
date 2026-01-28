439d029c3b069ccbe9058ed5cd6bcb48a2310e07
jeremie-star <j.iyamurinz@alustudent.com>
2026-01-28 03:38:32 -0800

    ft: fix

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,9 +1,52 @@
-
 #!/usr/bin/env python3
-import numpy as np
+"""
+12-bracin_the_elements.py
+Performs element-wise addition, subtraction, multiplication, and division
+on two lists or numbers without importing any modules.
+"""
+
 
 def np_elementwise(mat1, mat2):
-    """Performs element-wise addition, subtraction, multiplication, and division."""
-    return (np.add(mat1, mat2), np.subtract(mat1, mat2),
-            np.multiply(mat1, mat2), np.divide(mat1, mat2))
+    """
+    Performs element-wise addition, subtraction, multiplication, and division.
 
+    Args:
+        mat1 (list or int/float): First list/matrix or scalar.
+        mat2 (list or int/float): Second list/matrix or scalar.
+
+    Returns:
+        tuple: (sum, difference, product, quotient)
+    """
+    # Helper to determine if object is a list
+    def is_list(x):
+        return isinstance(x, list)
+
+    # If both are numbers
+    if not is_list(mat1) and not is_list(mat2):
+        return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
+
+    # If one is scalar, broadcast it
+    if not is_list(mat1):
+        mat1 = [mat1] * len(mat2)
+    if not is_list(mat2):
+        mat2 = [mat2] * len(mat1)
+
+    # Recursive element-wise operation
+    add = []
+    sub = []
+    mul = []
+    div = []
+
+    for a, b in zip(mat1, mat2):
+        if is_list(a) and is_list(b):
+            res = np_elementwise(a, b)
+            add.append(res[0])
+            sub.append(res[1])
+            mul.append(res[2])
+            div.append(res[3])
+        else:
+            add.append(a + b)
+            sub.append(a - b)
+            mul.append(a * b)
+            div.append(a / b)
+    return (add, sub, mul, div)
