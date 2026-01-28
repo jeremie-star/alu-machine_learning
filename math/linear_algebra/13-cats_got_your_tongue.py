472ace2e4288593c3cdbf85786bd2fc4cda47a7f
jeremie-star <j.iyamurinz@alustudent.com>
2026-01-28 03:33:26 -0800

    ft: added the read me file

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,8 +1,12 @@
-
 #!/usr/bin/env python3
+"""
+14-saddle_up.py
+Performs matrix multiplication on two numpy.ndarrays without loops.
+Returns a new matrix or None if multiplication is not possible.
+"""
 import numpy as np
 
 def np_cat(mat1, mat2, axis=0):
+
     """Concatenates two numpy arrays along a specified axis."""
     return np.concatenate((mat1, mat2), axis=axis)
-
